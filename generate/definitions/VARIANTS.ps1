5c56b8ed251a1ed38636a10d67b076817fa37555
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-25 15:59:57 +0000

    Refactor (ci): Use a `job_group_key` to group jobs

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -7,6 +7,7 @@ $VARIANTS = @(
             _metadata = @{
                 package_version = $v
                 platforms = 'linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/s390x'
+                job_group_key = $v
             }
             tag = "v$v"
             tag_as_latest = if ($v -eq $local:VERSIONS[0] ) { $true } else { $false }
